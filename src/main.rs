8faa6a23427bac7ad6ec959c04198ff3c42bf802
plu7o <skankhunt4ort2wo@gmail.com>
2023-07-21 01:26:34 +0200

    Heltix V.0.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
-#![allow(dead_code)]
-#![allow(unused)]
+#![warn(clippy::all, clippy::pedantic)]
 
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
+mod terminal;
 
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Postition;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
